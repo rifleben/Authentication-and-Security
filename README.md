6e7bd7f100b625fbbc55a9439815c7c59c1dbc13
rifleben <riflemab@oregonstate.edu>
2023-04-14 01:54:32 -0700

    adding level 2 requirements through mongoose-encryption

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ This is a simple application that I am using to learn about the various levels o
 * [Express](https://expressjs.com/)
 * [mongoDB](https://www.mongodb.com/)
 * [Mongoose](http://mongoosejs.com/)
+
+Started with level 1, which is just a simple login page, that uses a local db to store the info (mongoDB).
--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-    };
+    });
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
